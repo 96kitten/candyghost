c244dd3ff7508151dfa53946c601d146fd94bd1e
96kitten <niko05139625@gmail.com>
2017-11-19 15:31:03 +0900

    first commit

---
+++ cyandyghost/Assets/script/enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+	
+	float speed = 3.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2 (0.0f, speed * Time.deltaTime);
+	}
+}
