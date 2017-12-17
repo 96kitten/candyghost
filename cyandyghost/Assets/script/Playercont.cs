9755f85f65cdaa32174bf34f1f35af9e92b95786
96kitten <niko05139625@gmail.com>
2017-12-17 13:50:48 +0900

    HP減少設定追加

--- cyandyghost/Assets/script/Playercont.cs
+++ cyandyghost/Assets/script/Playercont.cs
@@ -5,12 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class Playercont : MonoBehaviour {
 	int hp;
+	public GameObject hitpoint;
+	public GameObject hitpoint2;
+	public GameObject hitpoint3;
+	public GameObject hitpoint4;
+	public GameObject hitpoint5;
 	void Start () {
 		hp = 5;
 	}
 
 	void Update () {
+		if (hp >= 5) {
+			hp = 5;
+			hitpoint.SetActive (true);
+			hitpoint2.SetActive (true);
+			hitpoint3.SetActive (true);
+			hitpoint4.SetActive (true);
+			hitpoint5.SetActive (true);
+		} else if (hp == 4) {
+			hitpoint.SetActive (false);
+			hitpoint2.SetActive (true);
+			hitpoint3.SetActive (true);
+			hitpoint4.SetActive (true);
+			hitpoint5.SetActive (true);
+		}else if (hp == 3) {
+			hitpoint.SetActive (false);
+			hitpoint2.SetActive (false);
+			hitpoint3.SetActive (true);
+			hitpoint4.SetActive (true);
+			hitpoint5.SetActive (true);
+		}else if (hp == 2) {
+			hitpoint.SetActive (false);
+			hitpoint2.SetActive (false);
+			hitpoint3.SetActive (false);
+			hitpoint4.SetActive (true);
+			hitpoint5.SetActive (true);
+		}else if (hp == 1) {
+			hitpoint.SetActive (false);
+			hitpoint2.SetActive (false);
+			hitpoint3.SetActive (false);
+			hitpoint4.SetActive (false);
+			hitpoint5.SetActive (true);
+		}
 		if (hp <= 0) {
+			hitpoint.SetActive (false);
+			hitpoint2.SetActive (false);
+			hitpoint3.SetActive (false);
+			hitpoint4.SetActive (false);
+			hitpoint5.SetActive (false);
 			SceneManager.LoadScene("Finish");
 		}
 	}
