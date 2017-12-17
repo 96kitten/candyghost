91bc082417af773c8f697e2b12d1ff5c60c629fa
96kitten <niko05139625@gmail.com>
2017-12-17 10:31:44 +0900

    エネミー　発生変更

--- cyandyghost/Assets/script/EnemyGenerator.cs
+++ cyandyghost/Assets/script/EnemyGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour {
 	float timer = 0.0f; 
-	float interval= 1.0f;
+	float interval= 1.5f;
 	public GameObject ene;
 	// Use this for initialization
 	void Start () {
@@ -15,7 +15,7 @@ public class EnemyGenerator : MonoBehaviour {
 	void Update () {
 		timer += Time.deltaTime;
 		if (timer >= interval) {
-			transform.position = new Vector3 (Random.Range(-2.7f,1.6f),5.6f);
+			transform.position = new Vector3 (Random.Range(-0.3f,1.6f),5.6f);
 			Instantiate(ene, transform.position,transform.rotation);
 			timer = 0;
 		}
