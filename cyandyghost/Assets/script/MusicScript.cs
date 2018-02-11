0de5c397f4e6e43d7408987d837373fc3a7ee46b
96kitten <niko05139625@gmail.com>
2018-02-11 13:15:02 +0900

    bgm追加

---
+++ cyandyghost/Assets/script/MusicScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour {
+
+	private AudioSource Candyeat;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D (Collision2D other){
+		if (other.gameObject.tag == "smallpoint") {
+			Candyeat.PlayOneShot (Candyeat.clip);
+		}
+	}
+}
--- cyandyghost/Assets/script/ScoreScript.cs
+++ cyandyghost/Assets/script/ScoreScript.cs
@@ -5,12 +5,20 @@ using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour {
 	public static int Point;
+	public static int highscore = 0;
 	public Text PointText;
+	public Text Highpoint;
 	void Start () {
 		
 	}
 
 	void Update () {
 		PointText.text = Point.ToString ();
+		if (Point > highscore) {
+			highscore = Point;
+			Highpoint.text = highscore.ToString();
+		}else if(Point <= highscore){
+			Highpoint.text = highscore.ToString();
+		}
 	}
 }
